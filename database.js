c96ad633023fc0ec1000a95982f287a18648507a
Chokealot <casper.konyi@iths.se>
2021-05-04 12:06:09 +0200

    Basic routes

---
+++ database.js
@@ -0,0 +1,33 @@
+var sqlite3 = require('sqlite3').verbose()
+
+const DBSOURCE = "question.db"
+
+
+let db = new sqlite3.Database(DBSOURCE, (err) => {
+    if (err) {
+        console.error(err.message)
+        throw err
+    }else{
+        console.log('Connected to the SQlite database.')
+        db.run(`CREATE TABLE question (
+            questionId INTEGER PRIMARY KEY,
+            questionTitle TEXT,
+            question,
+            answerOne TEXT,
+            answerTwo TEXT,
+            answerThree TEXT,
+            correctAnswer INTEGER
+            )`, (err) => {
+            if (err) {
+                console.log(err)
+            }
+            else {
+                var insert = 'INSERT INTO question (questionTitle, question, answerOne, answerTwo, answerThree, correctAnswer) VALUES (?,?,?,?,?,?)'
+                db.run(insert, ["Question One","What is the sum from: for(int x = 0; x<10; x++","0","9","10",2])
+            }
+        })
+    }
+})
+
+
+module.exports = db
--- server.js
+++ server.js
@@ -1,17 +1,117 @@
+/*SERVER DEFAULT CONFIGS*/
+
 var express = require("express")
 var app = express()
 var cors = require('cors')
 var db = require("./database.js")
+
 app.use(cors())
 app.use(express.static('public'))
+
 var bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
 var HTTP_PORT = 3000
-// Start server
+
 app.listen(HTTP_PORT, () => {
-    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+    console.log("Server running on port - %PORT%".replace("%PORT%",HTTP_PORT))
 });
+
+/*SERVER INDEX*/
 app.get("/", (req, res, next) => {
-    res.json({"message":"Ok"})
+    res.json("Server is running")
 });
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*SERVER ROUTING*/
+
+app.get("/api/question", (req, res, next) => {
+    var sql = "select * from question"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({"error":err.message});
+            return;
+        }
+        res.json(rows)
+    });
+});
+
+app.get("/api/question/:id", (req, res, next) => {
+    var sql = "select * from question where questionId = ?"
+    var params = [req.params.id]
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({"error":err.message});
+            return;
+        }
+        res.json(row)
+    });
+});
+
+app.post("/api/question/", (req, res, next) => {
+    var errors=[]
+    if (!req.body.questionTitle){
+        errors.push("No question title!");
+    }
+    var data = {
+        questionTitle: req.body.questionTitle,
+        question: req.body.question,
+        answerOne: req.body.answerOne,
+        answerTwo: req.body.answerTwo,
+        answerThree: req.body.answerThree,
+        correctAnswer: req.body.correctAnswer
+    }
+    var sql ='INSERT INTO question (questionTitle, question, answerOne, answerTwo, answerThree, correctAnswer) VALUES (?,?,?,?,?,?)'
+    var params =[data.questionTitle, data.question, data.answerOne, data.answerTwo, data.answerThree, data.correctAnswer]
+    db.run(sql, params, function (err, result) {
+        if (err){
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.json({
+            "message":"created",
+            "question": data,
+            "id" : this.lastID
+        })
+    });
+})
+
+app.put("/api/question/:id", (req, res, next) => {
+    var data = {
+        questionTitle: req.body.questionTitle,
+        question: req.body.question,
+        answerOne: req.body.answerOne,
+        answerTwo: req.body.answerTwo,
+        answerThree: req.body.answerThree,
+        correctAnswer: req.body.correctAnswer
+    }
+    var sql ='UPDATE question SET questionTitle = ?, question = ?, answerOne = ?, answerTwo = ?, answerThree = ?, correctAnswer = ? WHERE questionId = ?'
+    var params =[data.questionTitle, data.question, data.answerOne, data.answerTwo, data.answerThree, data.correctAnswer, req.params.id]
+    db.run(sql, params, function (err, result) {
+        if (err){
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.json({
+            "message":"updated",
+            "question": data,
+            "id" : this.lastID
+        })
+    });
+})
+
+app.delete("/api/question/:id", (req, res, next) => {
+    db.run(
+        'DELETE FROM question WHERE questionId = ?',
+        req.params.id,
+        function (err, result) {
+            if (err){
+                res.status(400).json({"error": res.message})
+                return;
+            }
+            res.json({"message":"deleted", rows: this.changes})
+        });
+})
